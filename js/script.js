5afbc63896c2d886c0638f055b0540f7b7fb0a84
Pyae Thein <pyaethein246@gmail.com>
2022-04-27 23:36:20 +0630

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+// ===== accordion ===== \\
 const accordionHeader = document.querySelectorAll('.faq__accordion--header'),
-accordionTab = document.querySelectorAll('.faq__accordion--tab');
-
+      accordionTab = document.querySelectorAll('.faq__accordion--tab');
 
 function toggleTab() {
   let itemClass = this.parentNode.className;
@@ -15,3 +15,27 @@ function toggleTab() {
 };
 
 accordionHeader.forEach(n => n.addEventListener('click', toggleTab));
+
+
+// ===== slider ===== \\
+const reviewNav = document.querySelector('.reviews__buttons'),
+      reviewBtns = document.querySelectorAll('.reviews__btn'),
+      reviewSlide = document.querySelector('.reviews__content');
+
+reviewBtns[0].onclick = function(){
+  reviewSlide.style.transform = 'translateX(0)';
+  for(i = 0; i < 2; i++){
+    reviewBtns[i].classList.remove('active');
+  }
+  this.classList.add('active');
+}
+
+reviewBtns[1].onclick = function(){
+  reviewSlide.style.transform = 'translateX(-25%)';
+  for(i = 0; i < 2; i++){
+    reviewBtns[i].classList.remove('active');
+  }
+  this.classList.add('active');
+}
+
+
